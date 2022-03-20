f73a74288ba87e1f2fc00ec9ae35a0f6fb3a9da9
Felix Uhl <felix.uhl@outlook.com>
2022-03-20 10:09:11 +0100

    Add gray[200]
    gray[100] was too light, gray[300] was too dark.

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ export const COLORS = {
   white: 'hsl(0deg, 0%, 100%)',
   gray: {
     100: 'hsl(185deg, 5%, 95%)',
+    200: 'hsl(187deg, 5%, 90%)',
     300: 'hsl(190deg, 5%, 80%)',
     500: 'hsl(196deg, 4%, 60%)',
     700: 'hsl(220deg, 5%, 40%)',
