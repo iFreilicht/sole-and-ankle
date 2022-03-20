e9e6519e1cddd8eb358dc993663025f2751fa0f6
Felix Uhl <felix.uhl@outlook.com>
2022-03-20 09:29:41 +0100

    Exercise 1

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -23,12 +23,16 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  padding: 8px 24px;
+  gap: 24px;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
 `;
 
 const MarketingMessage = styled.span`
+  margin-right: auto;
   color: ${COLORS.white};
 `;
 
